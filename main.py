74ee4d330410f9736a980f626b2055b8b488f23f
javaisgarbage <112577811+javaisgarbage@users.noreply.github.com>
2022-08-31 10:45:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,55 @@
 import requests
+import os
 
-def loadResponse(response, i1, i2):
-  #open('Response.txt','w').write(response) #easier indexing
-  start = response.index(i1)+len(i1)
-  x = start
-  while response[x] != i2: x += 1
-  return response[start:x]
+class Rolimons:
+  def get_inverse_index(response, i1, i2):
+    end = response.index(i1)
+    x = end
+    while response[x] != i2: x -= 1
+    return response[x+1:end]
 
-def getIDfromI(name):
-  response = str(requests.get('https://www.rolimons.com/itemtable').text)
-  #open('itemtable.txt','w').write(response) #easier indexing
-  end = response.index(name)-5
-  x = end
-  while response[x] != '"': x -= 1
-  return response[x+1:end+1]
+  def get_index(response, i1, i2):
+    start = response.index(i1)+len(i1)
+    x = start
+    while response[x] != i2: x += 1
+    return response[start:x]
+    
+  class User:
+    def __init__(self, username=None, id=None):
+      if username is None:
+        self.id = id
+        self.username = requests.get(f'https://users.roblox.com/v1/users/{id}').json()['name']
+      else:
+        self.username = username
+        self.id = self.query_user(username)
+      self.value, self.rap, self.inventory, self.trade_ads = self.get_metadata()
 
-def getIDfromP(name):
-  response = str(requests.get('https://rblx.trade/u/'+name).text)
-  #open('playerIndex.txt','w').write(response) #easier indexing
-  start = response.index('https://www.roblox.com/users/')+29
-  x = start
-  while response[x] != '/': x += 1
-  return response[start:x]
+    def query_user(self, username):
+      request = requests.get(f'https://users.roblox.com/v1/users/search?keyword={username}&limit=10').json()
+      return request['data'][0]['id']
 
-def loadResponseInverse(response, i1, i2):
-  end = response.index(i1)
-  x = end
-  while response[x] != i2: x -= 1
-  return response[x+1:end]
+    def get_metadata(self):
+      request = requests.get(f'https://www.rolimons.com/player/{self.id}')
+      with open('raw.txt','w') as file: file.write(request.text)
+      value = Rolimons.get_inverse_index(request.text, '],"rap":', ',')
+      trade_ad_count = Rolimons.get_index(request.text, '"trade_ad_count":', ',')
+      inventory = []
+      rap = 0
+      request = requests.get(f'https://inventory.roblox.com/v1/users/{self.id}/assets/collectibles?limit=100').json()
+      for item in request['data']:
+        rap += item['recentAveragePrice']
+        inventory.append(item['assetId'])
+        
+      return value, rap, inventory, trade_ad_count
+  
+  class Item:
+    def __init__(self, id):
+      self.id = id
+      self.raw = requests.get('https://www.rolimons.com/itemapi/itemdetails').json()['items']
+      self.name = self.raw[str(self.id)][0]
 
-class rolimons():
-  class item:
-    def __init__(self, name):
-      self.id = getIDfromI(name)
-      self.url = 'https://rolimons.com/item/'+self.id
-    def value(self):
-      try: return loadResponse(str(requests.get(self.url).text),'Value=',' ')
-      except: return 'None'
-    def rap(self):
-      return loadResponse(str(requests.get(self.url).text),'RAP=',' ')
-    def avaliable_copies(self):
-      return loadResponse(str(requests.get(self.url).text),'Available Copies=', ' ')
-    def premium_copies(self):
-      return loadResponse(str(requests.get(self.url).text),'Premium Copies=', '.')
-    def demand(self):
-      try: return loadResponse(str(requests.get(self.url).text),'Demand=', ' ')
-      except: return 'None'
-    def trend(self):
-      try: return loadResponse(str(requests.get(self.url).text),'Trend=', ' ')
-      except: 'None'
-
-  class player:
-    def __init__(self, name):
-      self.id = getIDfromP(name)
-      self.url = 'https://www.rolimons.com/player/'+self.id
-    def value(self):
-      return loadResponseInverse(str(requests.get(self.url).text),'],"rap":',',')
-    def rap(self):
-      return loadResponseInverse(str(requests.get(self.url).text),'],"num_limiteds":',',')
-    def items(self):
-      return loadResponseInverse(str(requests.get(self.url).text),'],"terminated":',',')
+      if self.raw[str(self.id)][3] == -1:
+        self.value = self.raw[str(self.id)][2]
+      else:
+        self.value = self.raw[str(self.id)][3]
+      self.rap = self.raw[str(self.id)][2]
